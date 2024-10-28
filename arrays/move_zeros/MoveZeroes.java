776843e972b0645b9d5e7a8e2465c44207de79e4
kiken-eng <kanyecope@gmail.com>
2024-10-29 00:31:29 +0300

    Shift zeros to the end of the digits

---
+++ arrays/move_zeros/MoveZeroes.java
@@ -0,0 +1,25 @@
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        if (nums.length == 1)
+            return;
+
+        int left = 0;
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] != 0) {
+                nums[left] = nums[right];
+                left++;
+            }
+        }
+        // Fill the rest of the array with 0
+        for (int i = left; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        MoveZeroes obj = new MoveZeroes();
+        int[] nums = { 0, 1, 0, 3, 12 };
+        obj.moveZeroes(nums);
+
+    }
+}
